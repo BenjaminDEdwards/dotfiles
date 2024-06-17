a4109c06fd63df43eb24d8b551efa0f1ac50bd37
Ben Edwards <mr.b.edwards@gmail.com>
2024-06-17 13:02:20 +0100

    add replace config

--- aws/updateAWSLoginCache.py
+++ aws/updateAWSLoginCache.py
@@ -8,9 +8,12 @@ import json
 import os
 import sys
 import argparse
+import yaml
+
 
 parser = argparse.ArgumentParser(description="Fetch the aws environment")
 parser.add_argument('pid', type=str, help="current pid")
+parser.add_argument('--config', type=str, help="config file", default="")
 parser.add_argument('--force', action='store_true', help='force cache refresh')
 
 args = parser.parse_args()
@@ -18,7 +21,6 @@ args = parser.parse_args()
 plaintext_file = f'/tmp/aws_current_env_{args.pid}'
 cache_file = f'/tmp/aws_env_cache_{args.pid}'
 
-print(f'FILE: {plaintext_file}')
 
 @dataclass(frozen=True)
 class CachedValue:
@@ -55,12 +57,30 @@ def fetchResponse():
     )
 
 
+@dataclass(frozen=True)
+class StringReplace:
+  match: str
+  replace_with: str
+
+def load_string_replace_from_yaml(file_path: str) -> list[StringReplace]:
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            data = yaml.safe_load(file)
+            return [StringReplace(**data)]
+    else:
+        return []
+
+def apply_replacements(replacements: list[StringReplace], input_string: str) -> str:
+    for replacement in replacements:
+        input_string = input_string.replace(replacement.match, replacement.replace_with)
+    return input_string
 
 # caller_identity = boto3.Session().client('sts').get_caller_identity()
 # account = caller_identity["Account"]
 
 # iam = boto3.Session().client('iam')
 
+replacements = load_string_replace_from_yaml(args.config)
 
 if not os.path.isfile(cache_file):
     cached = fetchResponse()
@@ -88,6 +108,8 @@ if ( now_ts > cached_value.expires or cached_value.success == False or args.forc
   cached = fetchResponse()
 
   with open(plaintext_file, 'w') as file:
+    replaced = apply_replacements(replacements,cached.env)
+    print(f'result: {cached.env} replaed to: {replaced}')
     file.write(cached.env)
 
   cached_value_dict = asdict(cached)
